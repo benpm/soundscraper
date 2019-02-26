ad1f7eb545ac394ca896fd26742198500e825c89
benpm <schwalegos@gmail.com>
2019-02-26 09:43:05 -0700

    visualization of fft

--- test_me.py
+++ test_me.py
@@ -1,14 +1,12 @@
 from pydub import AudioSegment
+import librosa
+from matplotlib import pyplot
+import numpy
 
-def extract_audio_data(audio_path, file_type):
-	if file_type != "wav" and file_type != "mp3":
-		print("File type must either be mp3 or wav, returning nulls")
-		return None, None, None
-	else:
-		sound = AudioSegment.from_file(audio_path, file_type)
-		return sound.get_array_of_samples(), sound.frame_rate, sound.channels
-
-raw_audio, sample_rate, num_channels = extract_audio_data("wilhelm.wav", "wav")
+raw_audio, sample_rate = librosa.load("wilhelm.wav", mono=True)
 print("Number of samples in audio data = %d" % len(raw_audio))
-print("Sample Rate = %d\nNumber of Channels = %d" % (sample_rate, num_channels))
 
+transformed_audio = librosa.stft(raw_audio, dtype=numpy.float)
+print(type(transformed_audio))
+pyplot.imshow(transformed_audio)
+pyplot.show()
