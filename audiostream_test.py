5fa2ff575b33ee73312f393d34b2b33360a1f32e
benpm <schwalegos@gmail.com>
2019-03-24 12:48:29 -0600

    trying to do stuff

--- audiostream_test.py
+++ audiostream_test.py
@@ -1,13 +1,20 @@
 import librosa
+import numpy
 import wave
 import pyaudio
 import time
 import sys
 
+position = 0
+
+def stream_callback(in_data, frame_count, time_info, status):
+    return (samples, pyaudio.paContinue)
+
 if len(sys.argv) < 2:
     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 
+CHUNK = 1024
 samples, rate = librosa.load("music.wav", mono=True)
 
 # instantiate PyAudio (1)
@@ -17,12 +24,13 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=rate,
-                output=True)
+                output=True,
+                stream_callback=stream_callback)
 
 # start the stream (4)
-stream.write(samples)
+#stream.write(samples, len(samples))
+stream.start_stream()
 
-# wait for stream to finish (5)
 while stream.is_active():
     time.sleep(0.1)
 
