3fd2faa5c73ba7021066da2fe337a633793761d4
benpm <schwalegos@gmail.com>
2019-04-19 11:20:40 -0600

    working with yoshimi

--- midi_interface.py
+++ midi_interface.py
@@ -6,7 +6,10 @@ import mido.ports
 
 class MIDIOutput(Output):
     def __init__(self):
-        self.port = mido.open_output()
+        self.port = mido.open_output(mido.get_output_names()[-1])
+        self.msg = mido.Message("note_on")
     
     def handler(self, label, start_time, length):
-        raise NotImplementedError
+        self.msg.note = random.randint(40, 110)
+        self.msg.channel = 0
+        self.port.send(self.msg)
--- test.py
+++
@@ -1,7 +0,0 @@
-import mido
-
-port = mido.open_output()
-
-midi_file = mido.MidiFile("test.mid")
-for msg in midi_file.play():
-    port.send(msg)
